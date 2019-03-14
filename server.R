dbb9cea54f590383fe1eb6d38ac7c130a00df6d6
benjamin-fornia <benfornia@gmail.com>
2019-03-14 12:41:12 -0700

    Added ben.server code to master server file

--- server.R
+++ server.R
@@ -9,7 +9,20 @@ shinyServer(function(input, output){
       geom_line(size = 2) +
       geom_point(size = 5)
   })
+  
   output$salary_plot <- renderPlotly({
     degree_comparison(input$degrees)
+  })
+  
+  output$plot_school_type <- renderPlot({
+    filtered <- type_aggregate %>%
+      filter(School.Type %in% c(input$school_1, input$school_2)) %>%
+      select(School.Type, input$bar_type)
+    
+    ggplot(data = filtered, aes(x = School.Type,
+                                y = pull(filtered, input$bar_type),
+                                fill = School.Type)) +
+      geom_bar(stat = "identity") +
+      ylab(input$bar_type)
   })
 })
